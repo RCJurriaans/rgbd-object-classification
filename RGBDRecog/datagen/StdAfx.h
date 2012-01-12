97dbadeb45086df9975650e06b3a8ddb5ee8a1d9
taco.cohen@gmail.com <taco.cohen@gmail.com@ce9f6871-04ee-7efe-acd5-70b66282a1c3>
2012-01-12 19:22:53 +0000

    Basic datagen functionality working: image (bmp) and depth (pcd) are saved. Need to fix some aliasing.

---
+++ RGBDRecog/datagen/StdAfx.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <targetver.h>
+
+#include <stdio.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+
+#include <tchar.h>
+
+#include <XnOS.h>
+#include <GL/glut.h>
+#include <math.h>
+#include <XnCppWrapper.h>
+
+#include <cv.h>
+#include <cxcore.h>
+#include <highgui.h>
+
+#include <pcl/point_cloud.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+//#include <pcl/io/openni_grabber.h>
+//#include <pcl/common/time.h>
+
+//#include <pcl/visualization/cloud_viewer.h>
