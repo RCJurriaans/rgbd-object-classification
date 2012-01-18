98c558a1dbb7ccd0772e3189b6f0865e58e842fd
robrechtjurriaans@gmail.com <robrechtjurriaans@gmail.com@ce9f6871-04ee-7efe-acd5-70b66282a1c3>
2012-01-18 09:45:24 +0000

    Forgot the actual code

---
+++ trunk/RGBDRecog/BackgroundSubtraction/stdafx.h
@@ -0,0 +1,16 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+//#include "targetver.h"
+
+#include <stdio.h>
+//#include <tchar.h>
+
+#include <pcl/point_cloud.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+#include <pcl/common/time.h>
