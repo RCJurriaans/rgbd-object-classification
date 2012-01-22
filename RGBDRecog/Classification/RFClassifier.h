6e1ae2f84f91e9ed21c815fe2aaf4c10b54b1a95
taco.cohen@gmail.com <taco.cohen@gmail.com@ce9f6871-04ee-7efe-acd5-70b66282a1c3>
2012-01-22 14:50:23 +0000

    Refactorings

--- RGBDRecog/Classification/RFClassifier.h
+++ RGBDRecog/Classification/RFClassifier.h
@@ -2,21 +2,22 @@
 
 //#include "ml.h"
 #include <opencv2\ml\ml.hpp>
+#include <string>
 using namespace std;
-//using namespace cv;
 
 class RFClassifier
 {
 public:
 	RFClassifier(void);
+	RFClassifier(string filename, string dataname);
 	~RFClassifier(void);
 
 	//takes an input cv::Mat, and classifies it with the random forest
 	//be sure that the amount of input parameters is the same
-	float predict(cv::Mat input);
+	int predict(cv::Mat input);
 
 	// takes a trainingData cv::Matrix, with for each cv::Matrix in the vector 
-	// containing the inforcv::Mation for each class
+	// containing the inforation for each class
 	// The predicted numbers will be integer numbers, that depend on the 
 	// ordering of the classes in the trainingData
 	// the amount of columns is the amount of datapoints
