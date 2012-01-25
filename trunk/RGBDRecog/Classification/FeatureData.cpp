e1f647684209d633739821b17a2580047f95b54d
TiRune@gmail.com <TiRune@gmail.com@ce9f6871-04ee-7efe-acd5-70b66282a1c3>
2012-01-25 00:05:36 +0000

    Added colorhistograms. Tested to work!

--- trunk/RGBDRecog/Classification/FeatureData.cpp
+++ trunk/RGBDRecog/Classification/FeatureData.cpp
@@ -5,7 +5,7 @@
 FeatureData::FeatureData(void)
 {
 	// edit the names and possible feature parameters for the whole function here!
-	amountOfFeatures = 6;
+	amountOfFeatures = 7;
 	featureNames = new string[amountOfFeatures];
 	featureNames[0] = "SIFT"; 
 	featureNames[1] = "Hue SIFT";
@@ -13,6 +13,7 @@ FeatureData::FeatureData(void)
 	featureNames[3] = "Normal SURF";
 	featureNames[4] = "Hue SURF";
 	featureNames[5] = "Opponent SURF";
+	featureNames[6] = "Color Histogram";
 }
 
 
